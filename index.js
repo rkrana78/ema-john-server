6ec761a99687d67da99d2b0061315c966022f139
rkrana78 <rashedul.karim7858@gmail.com>
2021-04-03 02:05:34 +0600

    create root api

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(bodyParser.json());
 app.use(cors());
 const port = 5000
 
-
+app.get('/', (req, res) => {
+  res.send("hello it's working")
+})
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
